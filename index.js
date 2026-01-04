c6271a47a634d70b397533c025f15a829b6a0837
armanaanie <viviandesena96@gmail.com>
2026-01-04 08:44:33 +0600

    js eddit

--- index.js
+++ index.js
@@ -1,3 +1,76 @@
-const form = document.getElementById('taskform');
-const input = document.getElementById('taskInput');
-const taskList= document.getElementById('taskList')
+const taskForm= document.getElementById('taskForm');
+const taskInput= document.getElementById('taskInput');
+const taskList= document.getElementById('taskList');
+const totalTask=document.getElementById('totalTask')
+function updateTotal(){
+    totalTask.innerText= taskList.children.length
+}
+function createTask(taskText){
+const li= document.createElement("li");
+const span=document.createElement("span");
+const actions=document.createElement("div");
+actions.className="actions";
+
+span.textContent=taskText;
+
+const doneBtn= document.createElement("button");
+doneBtn.innerText="Done";
+doneBtn.className="done-Btn";
+
+const deleteBtn= document.createElement("button");
+deleteBtn.innerText="Delete";
+deleteBtn.className="delete-Btn"
+
+
+const editBtn= document.createElement("button");
+editBtn.innerText="Edit";
+editBtn.className="edit-Btn"
+
+deleteBtn.addEventListener("click", ()=>{
+    li.remove()
+
+    updateTotal()
+});
+
+doneBtn.addEventListener("click",()=>{
+  span.classList.toggle("completed") ;
+updateTotal()
+
+});
+
+editBtn.addEventListener("click",()=>{
+    if(editBtn.innerText==="Edit"){
+        const input =document.createElement("input");
+        input.type="text";
+        input.value=text.innerText;
+
+        text.replaceWith(input);
+        input.id="text";
+
+        editBtn.innerText="Save";
+    }
+else{const input=document.getElementById("text");
+    const p=document.create
+})
+}
+actions.appendChild(doneBtn);
+actions.appendChild(deleteBtn);
+li.appendChild(span);
+li.appendChild(actions);
+taskList.appendChild(li);
+
+
+
+
+
+
+
+}
+taskForm.addEventListener("submit",(e)=>{
+    e.preventDefault();
+
+const taskText=taskInput.value
+
+  createTask(taskText);
+  input.value=""
+})
